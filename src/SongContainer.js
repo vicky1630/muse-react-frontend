0b7c03b3c2fdc25286f18331e75351c139eef3fc
Victoria_Cardwell <victoria.cardwell1@gmail.com>
2020-12-07 17:49:00 -0500

    react front end working - issue with .env file

---
+++ src/SongContainer.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import SongList from './SongList';
+
+class SongContainer extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      songs: []
+    }
+  }
+  componentDidMount(){
+    this.getSongs();
+  }
+  getSongs = async () => {
+    try {
+      const parsedSongs = await axios(
+          //process.env.REACT_APP_FLASK_API_URL + '/api/v1/songs/'
+        'http://localhost:8000/api/v1/songs/'
+      );
+      console.log(parsedSongs.data.data);
+      await this.setState({
+        songs: parsedSongs.data.data,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  render() {
+    return <SongList songs={this.state.songs} />;
+  }
+}
+
+export default SongContainer
---
+++ src/SongList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Card, Button} from 'semantic-ui-react';
+
+function SongList(props){
+  const songs = props.songs.map((song) => {
+    return (
+        <Card key={song.id}>
+          <Card.Content>
+            <Card.Header>{song.title}</Card.Header>
+            <Card.Header>{song.artist}</Card.Header>
+            <Card.Description>{song.album}</Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <Button>Delete Song</Button>
+            <Button>Edit Song</Button>
+          </Card.Content>
+        </Card>
+        )
+  })
+
+  return <Card.Group>{songs}</Card.Group>;
+}
+
+export default SongList;
