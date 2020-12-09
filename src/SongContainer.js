aebec4755b7b15ee9676d690c46a2f6ea8917a0b
Victoria_Cardwell <victoria.cardwell1@gmail.com>
2020-12-09 11:17:31 -0500

    edit modal

--- src/SongContainer.js
+++ src/SongContainer.js
@@ -3,15 +3,21 @@ import axios from 'axios';
 import SongList from './SongList';
 import CreateSongForm from './CreateSongForm';
 import { Grid } from 'semantic-ui-react';
+import EditSongModal from './EditSongModal';
 
 class SongContainer extends Component {
-  constructor(props){
-    super(props);
-
-    this.state = {
-      songs: []
-    }
+  
+  state = {
+    songs: [],
+    songToEdit: {
+      title: '',
+      artist: '',
+      album: '',
+      id: ''
+    },
+    showEditModal: false
   }
+
   componentDidMount(){
     this.getSongs();
   }
@@ -66,6 +72,50 @@ class SongContainer extends Component {
     console.log(deleteSongResponse, ' response from Flask server');
   };
 
+  openAndEdit = (songFromTheList) => {
+    console.log(songFromTheList, ' songToEdit  ');
+    this.setState({
+      showEditModal: true,
+      songToEdit: {
+        ...songFromTheList,
+      },
+    });
+  };
+
+  handleEditChange = (e) => {
+    this.setState({
+      songToEdit: {
+        ...this.state.songToEdit,
+        [e.currentTarget.name]: e.currentTarget.value,
+      },
+    });
+  };
+
+  closeAndEdit = async (e) => {
+    e.preventDefault();
+    try {
+      const editResponse = await axios.put(
+        process.env.REACT_APP_FLASK_API_URL +
+          '/api/v1/songs/' +
+          this.state.songToEdit.id,
+        this.state.songToEdit
+      );
+      console.log(editResponse, ' parsed edit');
+      const newSongArrayWithEdit = this.state.songs.map((song) => {
+        if (song.id === editResponse.data.data.id) {
+          song = editResponse.data.data;
+        }
+        return song;
+      });
+      this.setState({
+        showEditModal: false,
+        songs: newSongArrayWithEdit,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
 
   render() {
     return (    
@@ -73,11 +123,12 @@ class SongContainer extends Component {
       <Grid columns={2} divided textAlign='center' style={{ height: '100%' }} verticalAlign='top' stackable>
       <Grid.Row>
         <Grid.Column>
-          <SongList songs={this.state.songs} deleteSong={this.deleteSong}/>
+          <SongList songs={this.state.songs} deleteSong={this.deleteSong} openAndEdit={this.openAndEdit}/>
         </Grid.Column>
         <Grid.Column>
         <CreateSongForm addSong={this.addSong}/>
         </Grid.Column>
+        <EditSongModal handleEditChange={this.handleEditChange} open={this.state.showEditModal} songToEdit={this.state.songToEdit} closeAndEdit={this.closeAndEdit}/>
       </Grid.Row>
     </Grid>)
   }
